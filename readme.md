dfd74b9ec0cfafcb2322ce56a8c9a06072c32d6e
asaltcode <elangovanr558@gmail.com>
2023-12-02 16:42:23 +0530

    Task complete

---
+++ readme.md
@@ -0,0 +1,7 @@
+<img src="https://img.freepik.com/free-psd/emoji-element-isolated_23-2150355001.jpg" width="100" title="Asalt code" alt="Emoji">
+<h1>Gallery by Emoji API</h1>
+
+* This emoji API has more than thousand emojis but in this I am using only 200 images.
+* The reason for this is that the browser works slowly
+* Responsive for all devices
+* In this I am using CSS for Responsive Design
---
+++ script.js
@@ -0,0 +1,34 @@
+const emoji = document.getElementById("allEmoji");
+//It enables horizontal scrolling
+const pointerScroll = (elem) => {
+  let isDrag = false;
+  const dragStart = () => (isDrag = true);
+  const dragEnd = () => (isDrag = false);
+  const drag = (ev) => isDrag && (elem.scrollLeft -= ev.movementX);
+
+  elem.addEventListener("pointerdown", dragStart);
+  addEventListener("pointerup", dragEnd);
+  addEventListener("pointermove", drag);
+};
+
+//getting all the data from the API
+const respons = fetch("https://emojihub.yurace.pro/api/all");
+respons
+  .then((datas) => datas.json())
+  .then((data) => {
+    for (let i = 0; i < 200; i++) {
+      //Creating a new element
+      const emojiDiv = document.createElement("div");
+      emojiDiv.classList = "emoji-box";
+      emojiDiv.innerHTML = `<div class="emoji-pic">
+      <div class="emoji">${data[i].htmlCode[0]}</div>
+  </div>
+  <div class="text-box">
+      <h3 class="emoji-name">Name: ${data[i].name}</h3>
+      <span class="category">Category: ${data[i].category}</span>
+  </div>`;
+      emoji.append(emojiDiv);
+    }
+    //Select all text boxes
+    document.querySelectorAll(".text-box").forEach(pointerScroll);
+  });
